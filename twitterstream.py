aba794156e11ba4e707e3069fc6594dc4d7f5860
TaylorKolasinski <taylorkolasinski@gmail.com>
2014-08-16 19:31:23 -0400

    Update twitterstream.py

--- twitterstream.py
+++ twitterstream.py
@@ -1,8 +1,6 @@
 import oauth2 as oauth
 import urllib2 as urllib
 
-# See assignment1.html instructions or README for how to get these credentials
-
 api_key = "API KEY"
 api_secret = "API SECRET"
 access_token_key = "ACCESS TOKEN"
